2f6bbae59456d8aaca13c3951e5375235f5c5be7
Alvaro Segovia <alvarosego01@gmail.com>
2024-11-06 19:03:42 -0400

    first commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ */
+
+if (!defined('THEME_ROOT_PATH')) {
+    define('THEME_ROOT_PATH', __DIR__);
+}
+
+if (!defined('THEME_NAME')) {
+    define('THEME_NAME', 'portfolio');
+}
+
+// Load Composer dependencies.
+require_once THEME_ROOT_PATH . '/vendor/autoload.php';
+require_once THEME_ROOT_PATH . '/src/classes/index.php';
+require_once THEME_ROOT_PATH . '/src/StarterSite.php';
+
+Timber\Timber::init();
+
+// Sets the directories (inside your theme) to find .twig files.
+Timber::$dirname = [ 'templates', 'src/views' ];
+
+new StarterSite();

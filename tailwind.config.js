4ef82183e9ae128d44c075cca5d2c82bd49c2218
Alvaro Segovia <alvarosego01@gmail.com>
2024-11-06 20:26:51 -0400

    initial project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
         "./src/**/*.php",
         "./resources/**/*.{js,jsx,ts,tsx,vue}",
     ],
-
     theme: {
         extend: {
 
@@ -22,20 +21,21 @@ module.exports = {
                 '3xl': '2000px'
             },
             fontFamily: {
-
+                inter: ['Inter', 'sans-serif'],
+                'uncut-sans': ['Uncut Sans', 'sans-serif'],
             },
             fontSize: {
-                x: ['0.50rem', { lineHeight: '1.5' }],
                 xs: ['0.75rem', { lineHeight: '1.5' }],
                 sm: ['0.875rem', { lineHeight: '1.5715' }],
                 base: ['1rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
                 lg: ['1.125rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
                 xl: ['1.25rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
                 '2xl': ['1.5rem', { lineHeight: '1.415', letterSpacing: '-0.01em' }],
-                '3xl': ['2rem', { lineHeight: '1.3125', letterSpacing: '-0.01em' }],
-                '4xl': ['2.25rem', { lineHeight: '1.25', letterSpacing: '-0.01em' }],
+                '3xl': ['1.875rem', { lineHeight: '1.333', letterSpacing: '-0.01em' }],
+                '4xl': ['2.25rem', { lineHeight: '1.277', letterSpacing: '-0.01em' }],
                 '5xl': ['3rem', { lineHeight: '1.2', letterSpacing: '-0.01em' }],
-                '6xl': ['4rem', { lineHeight: '1.1562', letterSpacing: '-0.01em' }],
+                '6xl': ['3.75rem', { lineHeight: '1.166', letterSpacing: '-0.01em' }],
+                '7xl': ['5rem', { lineHeight: '1', letterSpacing: '-0.01em' }],
             },
 
             lineHeight: {
@@ -53,13 +53,6 @@ module.exports = {
                 wide: '0.01em',
                 wider: '0.02em',
                 widest: '0.4em',
-            },
-        },
-        variants: {
-            extend: {
-                backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-                textColor: ['responsive', 'hover', 'focus', 'active'],
-                borderColor: ['responsive', 'hover', 'focus', 'active'],
             },
         },
     },
